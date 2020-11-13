700cd2c8c7ac3636c2ceb85d34eb6076038c033d
unknown <tatojukranthi@gmail.com>
2020-11-13 02:00:40 -0500

    sample view

--- virtual-shop-ui/src/app/app.component.ts
+++ virtual-shop-ui/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'virtual-shop-ui';
+  listOfItems = [{ 'itemName': 'Milk', 'itemImageUrl' : 'https://lh3.googleusercontent.com/proxy/7Kvmt_iAsljpZP6mg8Tf0Gi-DRa1GpJ63ODhMOL-loJB_rs4MjHA4cSA6JcD9Ey1WWgLFctdGoQoufgHvUS60XvH0HmO9dPL7kAofgw_oMz9csgq3ncA8zZtbOTU5wuOFR3qYM6WhG3_Ax5leIeso997cbChaQ', 'itemLatestPrice' : '$1.95', 'brand': 'Barums' },  
+                { 'itemName': 'Eggs', 'itemImageUrl' : 'https://assets.tetrapak.com/static/publishingimages/find-by-food/rollup/food/eggs.jpg', 'itemLatestPrice' : '$2.35','brand': 'Tysons'},  
+                { 'itemName': 'Apples', 'itemImageUrl' : 'https://images.freshop.com/86164/0eb3ceb0d60091eab3496729e6eaadaf_medium.png', 'itemLatestPrice' : '$4.12','brand': 'Martins'},  
+  ]; 
+
 }
--- virtual-shop-ui/src/app/app.module.ts
+++ virtual-shop-ui/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatExpansionModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
